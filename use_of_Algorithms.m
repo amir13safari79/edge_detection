36776874537d41e8ea91c28f245c0da883a6a72c
amir13safari79 <am13sa79@gmail.com>
2021-02-02 23:38:38 +0330

    ''

---
+++ use_of_Algorithms.m
@@ -0,0 +1,15 @@
+%% sobel Algorithm is implemented in sobel function
+    % check operation of sobel algorithm for :
+    % (pic1_section1),(pic2_section1) & (pic3_section1)
+    
+    sobel('pic1_section1.jpg',1);
+    sobel('pic2_section1.jpg',1);
+    sobel('pic3_section1.jpg',1);
+    
+%% Kirsch Algorithm is implemented in Kirsch function
+    % check operation of Kirsch algorithm for :
+    % (pic1_section1),(pic2_section1) & (pic3_section1)
+    
+    Kirsch('pic1_section1.jpg',1);
+    Kirsch('pic2_section1.jpg',1);
+    Kirsch('pic3_section1.jpg',1);
